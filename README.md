18fde5edce4293b16afb20a30ea388889d6e6254
Nate Grigg <nate.grigg@endurance.com>
2021-03-23 18:15:56 -0600

    show steps on the process page

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ DESIGNRE: Design Reviews
 I want a process where we can review designs.  
 I think we can implement it with existing code-review tools.  
 But I wanted to experiment with explicit steps in the design process.  
+
+Next Steps
+------------------------
+- [x] Show steps on the **Process** page
+- [ ] Make admin view for lists of reviews
--- reviews/admin.py
+++ reviews/admin.py
@@ -5,5 +5,13 @@ from reviews.models import Design, Review, Step, Process, ProcessStep
 admin.site.register(Design)
 admin.site.register(Review)
 admin.site.register(Step)
-admin.site.register(Process)
 admin.site.register(ProcessStep)
+
+
+class ProcessStepInline(admin.TabularInline):
+    model = ProcessStep
+
+
+@admin.register(Process)
+class ProcessAdmin(admin.ModelAdmin):
+    inlines = [ProcessStepInline]
